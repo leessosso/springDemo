5a84fdf246250dda8c2a1fcff92ae62fff02992c
user <user@LAPTOP-V2M397DN>
2020-06-14 15:36:13 +0900

    노트북에서 수정

--- src/main/java/com/test/ex02/PersonTest.java
+++ src/main/java/com/test/ex02/PersonTest.java
@@ -13,14 +13,14 @@ public class PersonTest {
 		System.out.println("나이 : " + person1.getAge());
 		System.out.println("person1 : " + person1);
 		
-		System.out.println("================================");
+		System.out.println("====================");
 		
 		Person person2 = ctx.getBean("person" , Person.class);
 		System.out.println("이름 : " + person2.getName());
 		System.out.println("나이 : " + person2.getAge());
 		System.out.println("person2 : " + person2);
 
-		System.out.println("================================");
+		System.out.println("====================");
 		
 		//빈의 scope에 따라서 바뀜
 		if(person1.equals(person2)) {
